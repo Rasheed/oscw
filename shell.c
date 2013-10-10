b7b8c4bf89cce04e1ccf0315f26378c23a455a54
rwihaib <rasheedwihaib@gmail.com>
2013-10-10 12:49:08 +0100

    shell.h added

--- shell.c
+++ shell.c
@@ -1,10 +1,28 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "shell.h"
 
 int main(){
 	//Read PROFILE FILE
-
+	profileReader();
+	// prompt for input
+	// handle input.
 }
 
 void profileReader(){
-	
+   char ch;
+   char file_name[8] = "PROFILE";
+   FILE *pf;
+ 
+   pf = fopen(file_name,"r"); // read mode
+ 
+   if( pf == NULL )
+   {
+      exit(EXIT_FAILURE);
+   }
+  
+   while( ( ch = fgetc(pf) ) != EOF )
+      printf("%c",ch);
+ 	
+   fclose(pf);
 }
